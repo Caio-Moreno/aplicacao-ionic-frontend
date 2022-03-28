528a9e8656730a71e8556390a8fc3ed13f8a4dc5
Caio Moreno <cvinicius.moreno@outlook.com>
2022-03-27 21:57:50 -0300

    Tela escolha de pagamento

--- src/models/pedido.dto.ts
+++ src/models/pedido.dto.ts
@@ -3,8 +3,8 @@ import { PagamentoDTO } from "./pagamento.dto";
 import { RefDTO } from "./ref.dto";
 
 export interface pedidoDTO {
-    cliente: RefDTO;
-    enderecoDeEntrega: RefDTO;
-    pagamento: PagamentoDTO;
-    itens: ItemPedidoDTO[];
+    cliente: RefDTO,
+    enderecoDeEntrega: RefDTO,
+    pagamento: PagamentoDTO,
+    itens: ItemPedidoDTO[]
 }
